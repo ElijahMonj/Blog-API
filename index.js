323d3fefed2ce5e66f8387abba067a1401c307e1
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-01-02 19:59:05 +0800

    test wtf

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express=require('express')
 const app =express()
 const mongoose=require('mongoose')
 
+
 mongoose.connect("mongodb+srv://admin:admin@cluster0.3hfbcxb.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser:true,dbName:'Blog-API'})
 const db=mongoose.connection
 db.on('error', (error)=>console.error(error))
@@ -12,6 +13,8 @@ app.use(express.json())
 
 const blogsRouter = require('./routes/blogs')
 app.use('/blogs',blogsRouter)
-
+app.get('/', (req,res)=>{
+    res.send("WTF")
+})
 const port =process.env.PORT || 3000;
 app.listen(port, ()=>console.log("Server Started at port "+port))
