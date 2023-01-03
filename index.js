269230d76f037410e3e6a9e0791cc8ffe01df73a
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-01-03 19:49:40 +0800

    changed back

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express=require('express')
 const app =express()
 const mongoose=require('mongoose')
-const Blog=require('./models/blog')
+
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.3hfbcxb.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser:true,dbName:'Blog-API'})
 const db=mongoose.connection
@@ -12,18 +12,9 @@ db.once('open', ()=>console.error("Connected to database"))
 app.use(express.json())
 
 const blogsRouter = require('./routes/blogs')
-//app.use('/blogs',blogsRouter)
-//app.get('/', (req,res)=>{
-//    res.send("WTF")
-//})
-
-app.get('/', async (req,res)=>{
-    try {
-        const blogs= await Blog.find()
-        res.json(blogs)
-    } catch (err) {
-        res.status(500).json({message : err.message})
-    }
+app.use('/blogs',blogsRouter)
+app.get('/', (req,res)=>{
+    res.send("WTF")
 })
 const port =process.env.PORT || 3000;
 app.listen(port, ()=>console.log("Server Started at port "+port))
